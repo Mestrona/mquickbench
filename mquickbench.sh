9206b3339b7aeab477704f4e92d7c42d339d933b
Alexander Menk <menk@mestrona.net>
2022-02-10 16:00:56 +0100

    Formatting / Org of IP

--- mquickbench.sh
+++ mquickbench.sh
@@ -11,6 +11,9 @@ gb=1  # how much diskspace is needed (in GiB) - we set this to 1 by default now 
 echo -n "# Hostname "
 hostname -f 
 
+echo -n "Hoster: "
+curl ipinfo.io/org -s
+echo
 
 echo "## Specs"
 
@@ -19,32 +22,46 @@ echo "### CPU"
 echo -n "Number of Cores: "
 cat /proc/cpuinfo |grep processor|wc -l
 cat /proc/cpuinfo |grep "model name"|uniq
+echo
 
-echo "## CPU benchmark (single thread)"
+echo "## Config"
+
+echo -n "Mount Options: "
+mount | grep " / "
+echo
+
+echo "## Benchmarks"
+
+echo "### CPU benchmark (single thread)"
 
 # finding 20000 primes equals 321238 operations
 sysbench --num-threads=1 cpu --cpu-max-prime=20000 run|grep --color=never "total time:"|sed "s/.*total time:\\s*\(.*\)s/\1/" | awk '{ print 321238 / $1 " operations / second"}'
+echo
 
-echo "## Memory benchmark"
+echo "### Memory benchmark"
 sysbench --num-threads=1 memory --memory-block-size=1M --memory-total-size=100G run | grep --color=never transferred
+echo
 
 
 # Repare RW bench
 sysbench fileio --file-total-size=${gb}G --file-num=1024 prepare > /dev/null
 ulimit -n 65000
 
-echo "## Disk random read write benchmark"
+echo "### Disk random read write benchmark"
 sysbench --num-threads=1 fileio --file-total-size=${gb}G --file-num=1024 --file-test-mode=rndrw --max-time=300 --max-requests=0 --file-extra-flags=direct --file-fsync-freq=1 run |grep --color=never -A 2 Throughput
 sysbench fileio --file-total-size=${gb}G --file-num=1024 cleanup > /dev/null
+echo
 
 
-echo "## Sequential Disk write benchmark (${gb}GB) writing"
+echo "### Sequential Disk write benchmark (${gb}GB) writing"
 
 dd if=/dev/zero of=test.file bs=1M count=${gb}000 oflag=direct|grep -v records
+echo
 
-echo "## Sequential Disk read benchmark (${gb}GB) reading"
+echo "### Sequential Disk read benchmark (${gb}GB) reading"
 dd if=test.file of=/dev/null bs=1M count=${gb}000|grep -v records
+echo
 
 rm test.file
 
-
+echo "Finished."
